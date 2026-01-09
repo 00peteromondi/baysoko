67b8435d0d59d4a4e8ca911d3dd6c893c6775319
00peteromondi <00peteromondi@gmail.com>
2026-01-09 19:01:06 +0300

    Added numerous functionality

--- homabay_souq/urls.py
+++ homabay_souq/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('delivery/', include('delivery.urls')),
     
     # Social authentication
-    path('accounts/', include('allauth.urls')),
+    
     path('admin/webhook-config/', admin_views.configure_webhooks, name='admin_configure_webhooks'),
 ]
 
